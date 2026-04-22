84cba52acd5b1092554d3f1cfd32067509c47b31
Victor E. Chocho <vechocho@gmail.com>
2026-04-22 10:11:22 -0500

    Delete _includes/js/measure.js

--- _includes/js/measure.js
+++
@@ -1,9 +0,0 @@
-// _includes/js/measure.js
-function attachMeasurements() {
-    var script = document.createElement('script');
-    script.async = true;
-    script.defer = true;
-    script.setAttribute('data-website-id', '4234986e-a300-4cde-b838-f507bdd0fc43');
-    script.src = 'https://stats.chocho.dev/analytics'; 
-    document.head.appendChild(script);
-}
